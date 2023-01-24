c48814b60d38415596d6e236a9185cf8e7e72321
ariel.pineiro@iskaypet.com <arielsrv@gmail.com>
2023-01-24 18:40:16 +0100

    basepath

--- src/main/program.go
+++ src/main/program.go
@@ -9,6 +9,7 @@ import (
 
 // @title Golang Template API
 // @description This is a sample golang template api. Have fun.
+// @BasePath /
 func main() {
 	if err := app.Run(); err != nil {
 		log.Fatal(err)
