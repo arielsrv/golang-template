436898391826d98611706238c7e319fa7d0752df
ariel.pineiro@iskaypet.com <arielsrv@gmail.com>
2023-01-31 14:13:22 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-appName=go-fiber-app
+appName=myapp
 echo $appName
-heroku container:push web -a $appName
+
+#heroku container:push web -a $appName
+#heroku container:release web -a $appName
+#heroku logs --tail -a $appName
+
+docker buildx build --platform linux/amd64 -t $appName .
+docker tag $appName registry.heroku.com/$appName/web
+docker push registry.heroku.com/$appName/web
 heroku container:release web -a $appName
-heroku logs --tail -a $appName
