fbc04aa033b583e907d9e6a59838a880fe150d72
ariel.pineiro@iskaypet.com <arielsrv@gmail.com>
2023-01-31 16:42:29 +0100

    fix

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-appName=go-fiber-app
-echo "appName: " $appName
-
-docker buildx build --platform linux/amd64 -t $appName .
-docker tag $appName registry.heroku.com/$appName/web
-docker push registry.heroku.com/$appName/web
-heroku container:release web -a $appName
